e46b6562ead1039a0d9e5e087467a4e224e48879
Axel Aquino <aquino.axel@gmail.com>
2022-03-14 16:52:59 -0400

    commit tree 4ed900940469f0e5e9a4e46cec3c4e807cd27cd1

--- looping.rb
+++ looping.rb
@@ -1,5 +1,7 @@
 def happy_new_year
-  # your code here
+  (10..1).each do |i|
+    puts "#{i}"
+    puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
